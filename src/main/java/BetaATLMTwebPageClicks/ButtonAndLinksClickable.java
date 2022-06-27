8a356db6ea6e89b08d46dbe35b82b99e9583cbac
promitm83 <promitm83@gmail.com>
2022-06-27 19:42:36 +0530

    Commiting 3 27_06_2022

--- src/main/java/BetaATLMTwebPageClicks/ButtonAndLinksClickable.java
+++ src/main/java/BetaATLMTwebPageClicks/ButtonAndLinksClickable.java
@@ -4,6 +4,8 @@ import main.java.ConfigFile.ConfigAuto;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class ButtonAndLinksClickable extends ConfigAuto {
@@ -67,7 +69,7 @@ public class ButtonAndLinksClickable extends ConfigAuto {
         driver.navigate().back();
 
         JavascriptExecutor js1 = (JavascriptExecutor) driver;
-        js1.executeScript("window.scrollBy(0,900)");
+        js1.executeScript("window.scrollBy(0,1200)");
 
         try {
             Thread.sleep(2000);
@@ -76,6 +78,57 @@ public class ButtonAndLinksClickable extends ConfigAuto {
         }
 
         driver.findElement(By.xpath("//*[@id=\"main\"]/section[2]/div/div[3]/div/a")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        JavascriptExecutor js2 = (JavascriptExecutor) driver;
+        js2.executeScript("window.scrollBy(0,-1200)");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        driver.navigate().back();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        JavascriptExecutor js4 = (JavascriptExecutor) driver;
+        js4.executeScript("window.scrollBy(0,3000)");
+
+
+        WebElement ele1 = driver.findElement(By.xpath("//*[@id=\"main\"]/section[4]/div/div[2]/div/div/div[1]/div/h4"));
+        WebElement ele2 = driver.findElement(By.xpath("//*[@id=\"main\"]/section[4]/div/div[2]/div/div/div[2]/div/h4"));
+        WebElement ele3 = driver.findElement(By.xpath("//*[@id=\"main\"]/section[4]/div/div[2]/div/div/div[3]/div/h4"));
+
+        //Creating object of an Actions class
+        Actions action = new Actions(driver);
+
+        //Performing the mouse hover action on the target element.
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        action.moveToElement(ele1).perform();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        action.moveToElement(ele2).perform();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        action.moveToElement(ele3).perform();
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
